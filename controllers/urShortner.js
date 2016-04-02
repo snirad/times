999bb3fa205568ab8eb83651acdd747d7b657ad5
Snir Adam <snir.adam@ironsrc.com>
2016-04-02 20:34:47 +0300

    commit urlshortner

--- controllers/urShortner.js
+++ controllers/urShortner.js
@@ -8,6 +8,7 @@ const router = express.Router();
 
 
 function urlShortner(request, response) {
+    console.log(request);
     let prefix = request.params.query;
     if (parseInt(prefix)) {
         redirectURL(parseInt(prefix), response)
@@ -15,11 +16,11 @@ function urlShortner(request, response) {
         let url = request.url.slice(17);
         if (!validator.isURL(url)) return response.json({"Error": "Invalid URL"});
         let num = Randomnum.generateRandom();
-        let shorturl = request.headers.host + '/api/urlShortner/' + num;
+        let shorturl = 'https://' +request.headers.host + '/api/urlShortner/' + num;
         Postgre.queryPostgre(`insert into shorturl.url VALUES(${num},'${url}')`).then(()=> {
             return response.json({
                 "original_url": url,
-                "short_url": 'https://' + shorturl
+                "short_url": shorturl
             }).catch((err)=>response.json(err))
         })
     }
